1b03157c48998c94741c3744e6f7164b7ba543a9
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-04-25 22:29:00 +0700

    💚 E2E Tests template-vanilla-e2e fixed

--- tests/vanilla-e2e/cypress.config.ts
+++ tests/vanilla-e2e/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9753/',
+    baseUrl: 'http://localhost:5173/',
     video: false,
   },
 });
